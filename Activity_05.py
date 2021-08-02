1d47b083e6ada878a09a8af804c6491abc47daa4
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-02 18:50:58 +0530

    Update Activity_05.py

---
+++ Activity_05.py
@@ -0,0 +1,11 @@
+#List Stuff
+a = [int(x) for x in input().split()]
+print("Sum of all the numbers =",sum(a))
+
+#or
+# b = input().split()
+# sum1 = 0
+# for number in b:
+#     sum1+=int(number)
+
+# print("Sum of all the numbers is =",sum1)
