ea56a941681e507856d92e7d02ad0858af4dab8a
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-02 23:41:27 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -1,11 +1,29 @@
-print("Enter only numbers:\n")
-a = list(map(int,input().split()))
-# print(max(a))                           //easy method
+def inputa(): 
+    print("Enter only numbers:\n")
+    a = list(map(int,input().split()))
+    return a                          
 
-#or
-for first in range(len(a)):
-    for second in range(first+1,len(a)):
-        if(a[first]<a[second]):
-            a[first],a[second] = a[second],a[first]
+def easy_met(a):
+    return max(a)
+
+def if_else(a):
+    if a[0] >= a[1] and a[0] >= a[2]:
+        return a[0]
+    elif a[1]>=a[0] and a[1]>=a[2]:
+        return a[1]
+    else: 
+        return a[2]
         
-print("The Greatest Number is",a[0])      #Using Bubble sort
+def bubble_sort_mode(a):
+    for first in range(len(a)):
+        for second in range(first+1,len(a)):
+            if(a[first]<a[second]):
+                a[first],a[second] = a[second],a[first] #Using Bubble sort        
+    print("The Greatest Number is",a[0])      #Using Bubble sort
+    
+def main():
+    a = inputa()
+    print(easy_met(a),if_else(a),bubble_sort_mode(a))
+    
+
+main()
