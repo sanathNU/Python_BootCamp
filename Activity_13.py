82ecf38e7595e08565e842237be36974e34fd75a
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-02 22:14:30 +0530

    Done
    Done only classical way...may look into more efficient ways in the future

---
+++ Activity_13.py
@@ -0,0 +1,15 @@
+print("Enter only number:\n")
+a = int(input())
+if a == 0 or a == 1:
+    print("1 or 0 are not prime or composite numbers")
+else:
+    count = 0
+    numbers = []
+    for i in range(2,a-1):                  #The Brutefore way to check prime or not prime
+        if( a%i == 0):
+            count+=1
+            numbers.append(str(i))
+    if(count>0):
+        print("Not a prime!",a,"It's factors are ",','.join(numbers))
+    else:
+        print("It's a prime!",a)
