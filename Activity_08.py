ecf3e6580cdad92cfc00ecaec24e27fde2955581
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-02 19:45:02 +0530

    Update Activity_08.py

---
+++ Activity_08.py
@@ -0,0 +1,11 @@
+
+1)
+This term is a term related to computer language design wherein programming languages which are executed without being converted to machine or object code,
+or without the need for compiling the entire program beforehand are called interpreted languages. 
+
+2)
+An interpreter is a software program that executes a program without converting it to machine code or object code, while compiler is another software software wherein,
+the program is first converted to object code( like in JVM) or converted to machine code and then executed.
+
+3)
+The basic version of python is written in C language and is called CPython.
