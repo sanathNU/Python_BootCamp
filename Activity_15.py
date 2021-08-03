dc6d597bd81b6089971b39684ffb6e97a39ae96f
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-03 13:40:03 +0530

    Update Activity_15.py

---
+++ Activity_15.py
@@ -0,0 +1,21 @@
+#The main difference between sort and sorted is that
+# sort() is a method and sorted is a function
+# sort modifies the original list itself whereas sorted returns a new list of sorted values
+
+def input_string():
+    lis = [char for char in input().split()]
+    return lis
+
+def sort_using_meth(word):
+    word.sort()  # even if nothing is returned, the original list is modified
+    return word
+
+def sort_using_func(word):
+    return sorted(word)
+
+def main():
+    string1 = input_string()
+    print(sort_using_func(string1), string1) # printing sorted and original function....each list should be different
+    print(sort_using_meth(string1), string1) # since the original function is changed, both output should be the same
+
+main()
