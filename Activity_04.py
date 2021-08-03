89566384622e4a1397e11b826a12f0eb48c97f81
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-03 14:05:39 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -2,6 +2,8 @@
 a = int(input())
 b = int(input())
 
+# 3 methods of displaying the string
+print(a,"+",b,"=",a+b)
 print("{0} + {1} = {2}".format(a,b,a+b))   #format function using number lettering
 print("{x} + {y} = {z}".format(z = a+b, x =a, y = b)) #format function using string handlers
 print(a , "+",b,"= %d" %(a+b))            # using the old C format
