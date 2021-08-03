642b191c1c8016a551a4b07014cf43d2236bf04a
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-03 13:43:54 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -2,6 +2,6 @@
 a = int(input())
 b = int(input())
 
-print("{0} + {1} = {2}".format(a,b,a+b))
-print("{x} + {y} = {z}".format(z = a+b, x =a, y = b))
-print("%d + %d = %d" a b c)
+print("{0} + {1} = {2}".format(a,b,a+b))   #format function using number lettering
+print("{x} + {y} = {z}".format(z = a+b, x =a, y = b)) #format function using string handlers
+print(a , "+",b,"= %d" %(a+b))            # using the old C format
