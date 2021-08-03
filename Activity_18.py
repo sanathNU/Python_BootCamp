c34b6f115e1219aaf8c7d8c43f65c1a4d8c995a6
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-03 15:10:13 +0530

    Update Activity_18.py

--- Activity_18.py
+++ Activity_18.py
@@ -1,19 +1,20 @@
 # Given Input s=system;d=database;u=username;p=password
 
+#takes input
 def take_input():
     return input()
-
+#cleans input and gives out a string
 def parse_input(a):
     list1 = list(a.split(';'))
     list2 = [x.split('=') for x in list1]
     return list2
-
+#the input can be used to convert to dictionary
 def convert_dict(stuff):
     final_dict = {}
     for lists in stuff:
         final_dict[lists[0]] =lists[1]
     return final_dict
-
+#converting back from dictionary to string
 def challenge_part(dictionary):
     list1 =[]
     for key,value in dictionary.items():
@@ -21,6 +22,7 @@ def challenge_part(dictionary):
         list1.append(word)
     final_part = ';'.join(list1)
     return final_part
+
 def main():
     a = take_input()
     b = parse_input(a)          
