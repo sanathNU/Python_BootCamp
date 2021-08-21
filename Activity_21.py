7b00f333234322a4fe7590b059d5feadad01343f
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-21 12:21:36 +0530

    Got the correct ans
    Should clean up code that's it

--- Activity_21.py
+++ Activity_21.py
@@ -1,9 +1,20 @@
-#There are two approcahes in solving this problem
-#Both Have basic steps using calcluiton of days
 import time
 
 def GetEpochTime():
     return time.time()
+
+def meth1(D):
+    monthsTot = D//30
+    days   = D%30
+    years  = monthsTot//12
+    months = monthsTot%12
+    print(years,months,days)
+
+def meth2(D):
+    years = D//365
+    months = (D%365)//30
+    days   = D%30
+    print(years,months,days)
 
 def Diff(Seconds):
     #calculting minutes without seconds
@@ -11,28 +22,13 @@ def Diff(Seconds):
     #calcluting hours witohut minutes
     hours = mins//60
     #calculating days without hours
-    Days = hours//24
+    Days = int(hours//24)
     #here we reach an impasse, method 1 or method 2
+    print(Days)
+    meth1(Days)
+    meth2(Days)
 
-D_in_y = 365
-M_in_y = 12
-D_in_m = 30
-H_in_D = 24
-Min_in_h = 60
-S_in_min = 60
-
-mins = cur//60
-hours = mins//60
-days = hours//24
-day = days%30
-months = days//30
-years = months//12
-months = months%12
-# years = days//365
-# left_over = days%365
-# months = left_over//30
-# days = left_over%30
-print(years,months,day)
 
 if __name__=='__main__':
     Time = GetEpochTime()
+    Final = Diff(Time)
