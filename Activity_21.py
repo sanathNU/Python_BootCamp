e1a9d4e5d7a245ebbffc0371425af980ef021cf7
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-08-22 21:58:29 +0530

    final one
    added a little "real time" changes

--- Activity_21.py
+++ Activity_21.py
@@ -20,25 +20,36 @@ def meth2(D):
     print("It's been %d years,%d months and %d days since Unix Epoch"%(years,months,days))
     return years,months,days
 
-def CurrentDate(Years,Months,Days):
+def CurrentDate(Years,Months,Days,hours,minutes,seconds):
     Year = 1970+Years
     Month = Months
     Day = 1+Days
-    print("The Current Date is %d/%d/%d"%(Day,Month,Year))
+    hours+=5  #Adjusting for local time, it's just an extension to the earlier program
+    if(minutes+30 >60):
+        hours+=1
+        minutes-=30
+    else:
+        minutes+=30
+    print("The Current Date is %d/%d/%d- Time %d:%d:%d"%(Day,Month,Year,hours,minutes,seconds))
 
 def Diff(Seconds):
     #calculting minutes without seconds
     mins = Seconds//60
+    secs = Seconds%60
     #calcluting hours without minutes
     hours = mins//60
+    mini = mins%60
     #calculating days without hours
     Days = int(hours//24)
-    #here we reach an impasse, method 1 or method 2
+    hou = hours%24
+    #here we reach an impasse, method 1 or method 2...i'm choosing method 2
     y,m,d = meth2(Days)
-    return y,m,d
+    return y,m,d,hou,mini,secs
 
 
 if __name__=='__main__':
-    Time = GetEpochTime()
-    Y,m,d = Diff(Time)
-    CurrentDate(Y,m,d)
+    while(1):
+        Time = GetEpochTime()
+        Y,m,d ,h,mi,s= Diff(Time)
+        CurrentDate(Y,m,d,h,mi,s)
+        time.sleep(1.0)
